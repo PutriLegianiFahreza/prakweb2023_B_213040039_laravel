c6f2b684748fa7d8b80eebdc09cda355c1cf7214
PutriLegianiFahreza <putrilegiani234@gmail.com>
2023-11-05 19:07:58 +0700

    10. Factory & faker

---
+++ coba-laravel/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,85 @@
+<?php  
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
+class DatabaseSeeder extends DatabaseSeeder
+{
+
+    /**
+    *seed the aplication's Database
+    *
+    * @return void 
+    */
+    public function run()
+    {
+       User::factory(5)->create();
+
+    //    User::create([
+    //     'name' => 'putri legiani',
+    //     'email' => 'putrilegiani234@gmail.com',
+    //     'password' => bcrypt('12345')
+    //    ]);
+
+    //    User::create([
+    //     'name' => 'Rayhan',
+    //     'email' => 'rayhan234@gmail.com',
+    //     'password' => bcrypt('3456')
+    //    ]);
+
+
+    User::factory(3)->create();
+
+       Category::create([
+        'name' => 'Web Programming',
+        'slug' => 'Web Programming'
+       ]);
+
+       Category::create([
+        'name' => 'Personal',
+        'slug' => 'personal'
+       ]);
+
+       Post::factory(20)->create();
+
+    //    Post::create([
+    //     'title' => 'judul pertama',
+    //     'slug' => 'judul-pertama',
+    //     'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quo qui ipsa perferendis quaerat nostrum unde cupiditate porro adipisci autem?',
+    //     'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore incidunt ipsa possimus nemo beatae laboriosam inventore quas, ab voluptates cumque, iure tempore repellat accusamus. Hic, aperiam? Expedita beatae adipisci aspernatur.',
+    //     'category_id' => 1,
+    //     'user_id'=> 1
+    //    ]);
+
+    //    Post::create([
+    //     'title' => 'judul Kedua',
+    //     'slug' => 'judul-Kedua',
+    //     'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quo qui ipsa perferendis quaerat nostrum unde cupiditate porro adipisci autem?',
+    //     'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore incidunt ipsa possimus nemo beatae laboriosam inventore quas, ab voluptates cumque, iure tempore repellat accusamus. Hic, aperiam? Expedita beatae adipisci aspernatur.',
+    //     'category_id' => 1,
+    //     'user_id'=> 1
+    //    ]);
+
+    //    Post::create([
+    //     'title' => 'judul ketiga',
+    //     'slug' => 'judul-ketiga',
+    //     'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quo qui ipsa perferendis quaerat nostrum unde cupiditate porro adipisci autem?',
+    //     'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore incidunt ipsa possimus nemo beatae laboriosam inventore quas, ab voluptates cumque, iure tempore repellat accusamus. Hic, aperiam? Expedita beatae adipisci aspernatur.',
+    //     'category_id' => 2,
+    //     'user_id'=> 1
+    //    ]);
+
+    //    Post::create([
+    //     'title' => 'judul keempat',
+    //     'slug' => 'judul-keempat',
+    //     'excerpt' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis quo qui ipsa perferendis quaerat nostrum unde cupiditate porro adipisci autem?',
+    //     'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolore incidunt ipsa possimus nemo beatae laboriosam inventore quas, ab voluptates cumque, iure tempore repellat accusamus. Hic, aperiam? Expedita beatae adipisci aspernatur.',
+    //     'category_id' => 2,
+    //     'user_id'=> 2
+    //    ]);
+
+    }
+}
--- coba-laravel/resources/views/posts.blade.php
+++ coba-laravel/resources/views/posts.blade.php
@@ -9,8 +9,8 @@
 <article class="mb5 border-bottom pb-4">
 <h2> <a href="/posts/{{ $post->slug }}"> {{ $post->title }} </a></h2>
 
-<p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}"
-class="text-decoration-none">{{ $post->category->name }}</a> </p>
+<p>By. <a href="#" class="text-decoration-none">{{ $post->author->username }}</a> in <a href="/categories/{{ $post->category->slug }}"
+class="text-decoration-none">{{ $post->author->name }}</a> </p>
 
 <p> {{ $post->excerpt}} </p>
 
