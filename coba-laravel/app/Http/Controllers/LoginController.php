b4e97532c0dce10e35a8ef278d98252dff8f2cca
PutriLegianiFahreza <putrilegiani234@gmail.com>
2023-11-19 10:00:40 +0700

    16. user login & middleware

--- coba-laravel/app/Http/Controllers/LoginController.php
+++ coba-laravel/app/Http/Controllers/LoginController.php
@@ -11,7 +11,33 @@ class LoginController extends Controller
     public function index()
     {
         return view('login.index', [
-            'title' => 'Login'
+            'title' => 'Login',
+            'active' => 'login'
         ]);
     }
+
+    public function authenticate(Request $request) {
+        $credentials = $request->validate([
+            'email' => 'required|email:dns',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+
+       return back()->with('loginError', 'Login failed!');
+    }
+
+    public function logout() {
+        Auth::logout();
+
+        request()->session()->invalidate();
+
+        request()->session()->regenerateToken();
+
+        return redirect('/');
+    }
+
 }
---
+++ coba-laravel/resources/views/dashboard/index.blade.php
@@ -0,0 +1,1 @@
+<h1>Welcome, Putri Legiani</h1>
